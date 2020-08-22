4657bb7cb82be61f727453e1531b4c34818a1f9b
Brandon Bews <brandonbews@gmail.com>
2020-08-22 11:36:21 -0700

    Clean out Gatsby starter stuff, and personalize config files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Brandon Bews Portfolio`,
+    description: `This is my personal portfolio`,
+    author: `@brandonbews`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -19,22 +19,22 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Brandon Bews Portfolio`,
+        short_name: `Bews`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         typekit: {
-          id: 'xzm6jku'
-        }
-      }
+          id: 'xzm6jku',
+        },
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
