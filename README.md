97796437c388be30eeeb4f44f0c67d394f326268
Brandon Bews <brandonbews@gmail.com>
2020-08-23 20:51:41 -0700

    Additional cleanup of Gatsby starter theme.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Brandon Bews Personal Portfolio
+
+## Typography
+
+Adjust type system settings in \_typograhpy.scss
+
+### Font Weight Custom Properties
+
+- --fontWeightNormal: 400;
+- --fontWeightBold: 700;
+- --fontWeightBlack: 900;
+
+### Mixins to Create Font Sizes
+
+- **bodyFont(\$size)**
+- **titleFont(\$size)**
+  Headlines, titles, large copy
+- **uiFont(\$size)**
+  Title case UI elements
+- **uiFontUpper(\$size)**
+  Uppercase UI elements with a padding shim to better center text vertically
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Brandon Bews Portfolio`,
+    title: `brandon-bews-portfolio`,
     description: `This is my personal portfolio`,
     author: `@brandonbews`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Brandon Bews Portfolio`,
-        short_name: `Bews`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
@@ -36,8 +15,5 @@ module.exports = {
         },
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
