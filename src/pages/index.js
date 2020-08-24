b544c8cff8a51b70188b9ced6f8226d126504ccd
Brandon Bews <brandonbews@gmail.com>
2020-08-23 21:56:08 -0700

    Fixes in typography system: - Infinite while loop - Poorly scoped base font size variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const IndexPage = () => <Layout>{/* <SEO title="Home" /> */}</Layout>
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+  </Layout>
+)
 
 export default IndexPage
